266c91689581143d8cbe180e58f1d182190c067d
jiyoung <jenn4843@naver.com>
2025-11-20 03:21:23 +0900

    FIX: 단어장 생성 페이지 경로 지정

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,17 @@ import { BrowserRouter, Routes, Route } from 'react-router';
 import Album from './features/MusicChart/components/Album';
 import AlbumStack from './features/MusicChart/components/AlbumStack';
 import LoadingPage from './pages/LoadingPage';
+import VocabularyPage from './pages/VocabularyPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/create" element={<VocabularyPage />} />
         <Route path="/loading" element={<LoadingPage />} />
       </Routes>
     </BrowserRouter>
-    
   )
 }
 
-export default App
+export default App;
